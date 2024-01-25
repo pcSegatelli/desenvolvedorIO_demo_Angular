d49c7d40f92f4696ca6602571e7bc1441f1e380b
Paula Segatelli <pc.segatelli@gmail.com>
2024-01-25 13:11:32 -0300

    criando novas paginas

--- MeuProjeto/src/app/app.component.ts
+++ MeuProjeto/src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl:'./app.component.html' 
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'MeuProjeto';
