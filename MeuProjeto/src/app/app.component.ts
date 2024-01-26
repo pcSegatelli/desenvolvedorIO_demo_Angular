d73666abe035d0267891649a46f4b900f899325f
Paula Segatelli <pc.segatelli@gmail.com>
2024-01-26 00:15:26 -0300

    ajuste configuracao

--- MeuProjeto/src/app/app.component.ts
+++ MeuProjeto/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/enviroment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  constructor() {
+    console.log(environment.production); // Logs false for development environment
+  }
   title = 'MeuProjeto';
 }
